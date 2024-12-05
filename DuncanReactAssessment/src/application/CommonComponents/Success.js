209364f9d99fe39d28842c7ac27a164256895bb9
Duncan Asplundh <drasplundh@gmail.com>
2024-12-04 22:02:23 -0500

    added Q2 & 7

--- DuncanReactAssessment/src/application/CommonComponents/Success.js
+++ DuncanReactAssessment/src/application/CommonComponents/Success.js
@@ -28,6 +28,12 @@ export class Success extends React.Component {
             <>
                 <h1>{this.state.message}</h1>
                 <SuccessChild parentMessage={successParentData} storyComponent={<SuccessStory sendToSuccess={this.handleStoryMessage}/>} />
+                <p> I'm going to answer Q2 and Q7 together because they're similar</p>
+                <p>React uses a Virtual DOM which is a higher layer than the actual DOM.  As the programmer you interact with the Virtual DOM. <br></br>
+                When you make changes to it, React will identify those changes made to the Virtual DOM from the previous one using a really efficient diffing algorithm <br></br>and then reflect those changes in the actual DOM.  
+                <br></br>By using this Virtual DOM, React can pick and choose which parts of the actual DOM to update, rather than rendering the entire DOM each time a change is made; making it more efficient.
+                </p>
+
             </>
         )
     }
